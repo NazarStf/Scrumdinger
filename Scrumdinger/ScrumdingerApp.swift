516e863d1a6249e142b4571b8bd11e23a52f6121
NazarStf <hworker98@gmail.com>
2023-08-22 12:11:11 +0300

    Pass a binding into the list view

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-    var body: some Scene {
-        WindowGroup {
-			ScrumsView(scrums: DailyScrum.sampleData)
-        }
-    }
+	@State private var scrums = DailyScrum.sampleData
+	
+	var body: some Scene {
+		WindowGroup {
+			ScrumsView(scrums: $scrums)
+		}
+	}
 }
