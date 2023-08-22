689000b8d495e4e707151ed3744c49c90ccf17ce
NazarStf <hworker98@gmail.com>
2023-08-22 14:45:14 +0300

    Create AVPlayer+Ding.swift

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
 	@Binding var scrum: DailyScrum
 	@StateObject var scrumTimer = ScrumTimer()
+	
+	private var player: AVPlayer { AVPlayer.sharedDingPlayer }
 	
 	var body: some View {
 		ZStack {
@@ -21,20 +24,16 @@ struct MeetingView: View {
 								  theme: scrum.theme)
 				Circle()
 					.strokeBorder(lineWidth: 24)
-				HStack {
-					Text("Speaker 1 of 3")
-					Spacer()
-					Button(action: {}) {
-						Image(systemName: "forward.fill")
-					}
-					.accessibilityLabel("Next speaker")
-				}
+				MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
 			}
 		}
 		.padding()
 		.foregroundColor(scrum.theme.accentColor)
 		.onAppear {
 			scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+			scrumTimer.speakerChangedAction = {
+				player.seek(to: .zero)
+			}
 			scrumTimer.startScrum()
 		}
 		.onDisappear {
